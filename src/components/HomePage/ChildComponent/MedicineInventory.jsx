b254d7115980cb76c8601a5a2d6fca1bda925a08
birukGetachew <burab1742@gmail.com>
2024-10-06 10:16:00 -0700

    slice resolved

--- src/components/HomePage/ChildComponent/MedicineInventory.jsx
+++ src/components/HomePage/ChildComponent/MedicineInventory.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
-import { updateQuantity } from '../../../reducer/MedicineSlice';
+import { updateQuantity } from '../../../Reducer/MedicineSlice';
 import Barcode from 'react-barcode';
 import { Tabs, Layout, Button, Card, Form, Input, Select, Table } from 'antd';
 import { InfoCircleOutlined, HistoryOutlined, DollarOutlined } from '@ant-design/icons';
